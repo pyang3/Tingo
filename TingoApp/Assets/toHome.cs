d1f722015d67001b8747a3a24beb9de85ffb507a
bseibo61 <ben.seibold@gmail.com>
2018-02-21 12:44:01 -0500

    Added back button

---
+++ TingoApp/Assets/toHome.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class toHome : MonoBehaviour {
+
+	public void backPressed(string loadScene){
+		Debug.Log("Pressed");
+		SceneManager.LoadScene(loadScene);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
