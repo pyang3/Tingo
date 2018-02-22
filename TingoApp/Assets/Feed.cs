6fa0ce19421664dbe0150e38fb3fdab4de21e793
bseibo61 <ben.seibold@gmail.com>
2018-02-22 16:00:58 -0500

    Added dialogue boxes, messed with health system

--- TingoApp/Assets/Feed.cs
+++ TingoApp/Assets/Feed.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -39,6 +40,7 @@ public class Feed : MonoBehaviour {
 		if (sliderObj != null) {
 			Slider healthSlider = sliderObj.GetComponent<Slider>();
 			var health = healthSlider.value;
+			PlayerPrefs.SetFloat ("health", health);
 			if (health <= 0) {
 				Debug.Log ("Death");
 			}
@@ -46,9 +48,35 @@ public class Feed : MonoBehaviour {
 	
 	}
 
-	// Add berries for testing
-//	void Start () {
-//		PlayerPrefs.SetInt("berries", 3);
-//	}
+	 //Add berries for testing
+	public void Start () {
+		PlayerPrefs.SetInt("berries", 5);
+		Debug.Log ("Start");
+		GameObject.Find ("HealthSlider").GetComponent <Slider> ().value = .5f;
+
+		//get time as ascii value
+		int d1 = DateTime.Now.Hour;
+		System.DateTime epochStart = new System.DateTime(2018, 1, 1, 0, 0, 0, System.DateTimeKind.Utc);
+		int cur_time = (int)(System.DateTime.UtcNow - epochStart).TotalHours;
+
+		//check last time health was calculated
+		PlayerPrefs.GetFloat ("timeOfLastHealthCheck");
+		float healthDeduction = PlayerPrefs.GetFloat ("health") - (.05f * (cur_time - PlayerPrefs.GetFloat ("timeOfLastHealthCheck")));
+
+		//reduce health
+		PlayerPrefs.SetFloat ("health", healthDeduction);
+		PlayerPrefs.SetFloat ("timeOfLastHealthCheck", cur_time);
+
+		//Load scene from PlayerPrefs
+		GameObject sliderObj = GameObject.Find ("HealthSlider");
+		//		GameObject.Find ("HealthSlider").GetComponent <Slider> ().value = .5f;
+		if (sliderObj != null) {
+			Slider healthSlider = sliderObj.GetComponent<Slider> ();
+			Debug.Log (PlayerPrefs.GetFloat ("health"));
+			healthSlider.value = PlayerPrefs.GetFloat ("health");
+		} else {
+			Debug.Log ("Slider is null");
+		}
+	}
 
 }
---
+++ TingoApp/Assets/dialogue.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class dialogue : MonoBehaviour {
+	int clicks = 0;
+
+	public void onClick(){
+		if (clicks == 0) {
+			GameObject.Find ("Dialogue").GetComponentInChildren<Text> ().text = "Tingo: You can get more berries to feed me with by walking and getting enough steps throughout the day.";
+			clicks++;
+		} else {
+			GameObject.Find ("Dialogue").SetActive(false);
+		}
+	}
+}
