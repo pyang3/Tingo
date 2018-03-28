6ec4ad2e68a4e6e29fc38db06cff0d4ecfbbdf80
bseibo61 <ben.seibold@gmail.com>
2018-03-28 14:08:32 -0400

    Changed some values back from testing vals

--- TingoApp/Assets/Feed.cs
+++ TingoApp/Assets/Feed.cs
@@ -10,10 +10,10 @@ public class Feed : MonoBehaviour {
 	float berryValue = .1f;				//How much berries raise health by
 	float healthDrainRate = .027f;		//How much health is lost per hour (this takes 36 houres to starve)
 
-	int maxSteps = 10; 				// After this ammount of steps has been hit in a day, the rest will go to XP || DEFAULT:5000
-	int stepsPerBerry = 5; 			// Steps needed to get a berry || DEAFUALT : 500
+	int maxSteps = 5000; 				// After this ammount of steps has been hit in a day, the rest will go to XP || DEFAULT:5000
+	int stepsPerBerry = 500; 			// Steps needed to get a berry || DEAFUALT : 500
 
-	int steps = 6000;				//This will be replaced with the actual steps that were gathered that day
+	int steps = 3000;				//This will be replaced with the actual steps that were gathered that day
 
 	private PedometerPlugin pedometerPlugin;
 
@@ -67,12 +67,12 @@ public class Feed : MonoBehaviour {
 	//Runs each time game starts
 	public void Start () {
 		
-		pedometerPlugin = PedometerPlugin.GetInstance ();
-		pedometerPlugin.SetDebug (0);
-
-		pedometerPlugin.Init ();
-		pedometerPlugin.StartPedometerService (SensorDelay.SENSOR_DELAY_FASTEST);
-		steps = pedometerPlugin.GetStepToday ();
+//		pedometerPlugin = PedometerPlugin.GetInstance ();
+//		pedometerPlugin.SetDebug (0);
+//
+//		pedometerPlugin.Init ();
+//		pedometerPlugin.StartPedometerService (SensorDelay.SENSOR_DELAY_FASTEST);
+//		steps = pedometerPlugin.GetStepToday ();
 
 
 //		PlayerPrefs.SetInt ("steps",3000);
@@ -99,8 +99,8 @@ public class Feed : MonoBehaviour {
 
 		if (steps > maxSteps) {
 			//find the total number of xp that needs to be added
-			float addedXP = (float)((double)newSteps * .01); //CHANGE TO .0001
-
+			float addedXP = (float)((double)newSteps * .0001); //DEFAULT .0001
+			Debug.Log("steps:"+steps+" maxsteps "+maxSteps);
 			if ((PlayerPrefs.GetFloat ("xp") + addedXP) > 1) {
 				float xpToLevel = 1 - PlayerPrefs.GetFloat ("xp");
 				addedXP = addedXP - xpToLevel;
@@ -110,6 +110,7 @@ public class Feed : MonoBehaviour {
 			PlayerPrefs.SetFloat ("xp", PlayerPrefs.GetFloat ("xp") + addedXP);
 			PlayerPrefs.SetInt ("steps", PlayerPrefs.GetInt ("steps") + newSteps);
 		} else {
+			Debug.Log ("Adding to berries");
 			//find the number of new berries to add
 			int newBerries = newSteps / stepsPerBerry;
 			PlayerPrefs.SetInt ("berries",PlayerPrefs.GetInt("berries")+newBerries);
